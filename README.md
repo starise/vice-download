f3aaed8755db1fbbd16d498b733cb78618a7b75a
Andrea Brandi <me@andreabrandi.com>
2024-02-25 14:51:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Save Vice Articles
+
+## Requirements
+
+- [Python](https://www.python.org/) >= 3.12
+
+## Usage
+
+```python
+pip install -r requirements
+# Example:
+# For "https://www.vice.com/it/contributor/federico-nejrotti"
+# Use "federico-nejrotti" as argument
+python vice-download.py federico-nejrotti
+```
+
+## Output
+
+```txt
+There are 684 articles in memory
+List of all articles saved in file 'articoli.txt'
+```
---
+++ vice-download.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+from markdownify import markdownify as md
+
+
+CONTRIBUTOR = "".join(sys.argv[1:])
+
+# Find links to all articles
+author_page = "https://www.vice.com/it/contributor/" + CONTRIBUTOR
+
+# Save list of all articles in a text file
+def save_articles_list():
+    response = requests.get(author_page)
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a", class_="vice-card-hed__link", href=True)
+    pages = soup.find("span", class_="pagination_pager__progress-last-page").get_text()
+
+    author_articles = []
+    for page in range(1, int(pages) + 1):
+        for link in links:
+            author_articles.append(link["href"])
+
+    print("There are " + str(len(author_articles)) + " articles in memory")
+
+    with open("articoli.txt", "w") as f:
+        f.write("\n".join(map(str, author_articles)))
+
+    print("List of all articles saved in file 'articoli.txt'")
+
+save_articles_list()
